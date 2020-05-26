940e873c07a30045022d12e8ba08236ecb33fc3e
alexandre <amenezes031@gmail.com>
2020-05-26 20:53:14 -0300

    Iniciação do projeto.

---
+++ src/test/java/br/com/flf/aluguel/ProjetoAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flf.aluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
