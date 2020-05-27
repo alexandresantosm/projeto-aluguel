7fdf797045f507daf907964b3467a98ab76d9480
alexandre <amenezes031@gmail.com>
2020-05-26 21:26:45 -0300

    Adicão de anotação @Repository na classe de repositório Cliente.

--- src/main/java/br/com/flf/aluguel/repository/AluguelRepository.java
+++ src/main/java/br/com/flf/aluguel/repository/AluguelRepository.java
@@ -1,9 +1,11 @@
 package br.com.flf.aluguel.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.flf.aluguel.model.Aluguel;
 
+@Repository
 public interface AluguelRepository extends JpaRepository<Aluguel, Integer> {
 
 }
